7c3f6eba16b1420c26eb413ba9f5bc97a2fafe6a
Thomas Rovayaz <thomas.rovayaz74@gmail.com>
2021-05-15 14:42:31 +0200

    Localization module

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import {
   Button,
   SafeAreaView,
@@ -10,10 +10,30 @@ import {
 } from 'react-native';
 import database from '@react-native-firebase/database';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import * as RNLocalize from 'react-native-localize';
 import {Navigation, NavigationFunctionComponent} from 'react-native-navigation';
+import {setI18nConfig, translate} from '../locales';
 
-const HomeScreen: NavigationFunctionComponent = ({componentId}) => {
+const HomeScreen: NavigationFunctionComponent = ({
+  componentId,
+}: {
+  componentId: string;
+}) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
+
+  const handleLocalizationChange = () => {
+    setI18nConfig();
+    forceUpdate();
+  };
+
+  useEffect(() => {
+    setI18nConfig();
+    RNLocalize.addEventListener('change', handleLocalizationChange);
+    return () => {
+      RNLocalize.removeEventListener('change', handleLocalizationChange);
+    };
+  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -26,6 +46,7 @@ const HomeScreen: NavigationFunctionComponent = ({componentId}) => {
       console.log('User data: ', snapshot.val());
     })
     .catch(console.log);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -36,7 +57,7 @@ const HomeScreen: NavigationFunctionComponent = ({componentId}) => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Text>Home</Text>
+          <Text>{translate('hello')}</Text>
           <Button
             title="Create event"
             color="#710ce3"
